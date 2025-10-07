2d64b227f1d6ec980818f64cf325ceb27afff6e9
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-10-06 21:57:15 -0500

    Add access policy to s3 bucket

--- tf/main.tf
+++ tf/main.tf
@@ -17,3 +17,19 @@ resource "aws_s3_bucket_public_access_block" "allow_all_access" {
   restrict_public_buckets = false
 }
 
+resource "aws_s3_bucket_policy" "allow_get_access" {
+  bucket = aws_s3_bucket.learning_bucket.id
+  policy = <<EOP
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "${aws_s3_bucket.learning_bucket.arn}/*"
+    }
+  ]
+}
+EOP
+}
