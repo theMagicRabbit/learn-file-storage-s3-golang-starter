e3d205c290b3469aafd5fd0d5a9d1ae0a8b01425
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-10-13 13:59:55 -0500

    Add AWS functionality
    Squashed commit of the following:
    
    commit 5d814ce209e0f9dc1fc90af35b4bbdb43efb9edd
    Author: Benjamin Toll <themagicrabbit3of6@gmail.com>
    Date:   Mon Oct 13 13:17:00 2025 -0500
    
        Add video upload to s3
    
    commit 6916e145c78892575cfa8bdda7d6c1fa7c582598
    Author: Benjamin Toll <themagicrabbit3of6@gmail.com>
    Date:   Tue Oct 7 16:16:18 2025 -0500
    
        Uploade videos to aws
    
    commit d8a0482916a2bd5adfd7050f5fdd7e3d755b723f
    Author: Benjamin Toll <themagicrabbit3of6@gmail.com>
    Date:   Tue Oct 7 11:39:50 2025 -0500
    
        Add aws sdk

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,7 +1,121 @@
 package main
 
 import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	videoIDString := r.PathValue("videoID")
+	videoID, err := uuid.Parse(videoIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+		return
+	}
+
+	videoMetaData, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't find video meta data", err)
+		return
+	}
+
+	if videoMetaData.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Video is not owned by user", err)
+		return
+	}
+
+	maxFileSizeBytes := 1 << 30
+	http.MaxBytesReader(w, r.Body, int64(maxFileSizeBytes))
+	videoData, videoHeader, err := r.FormFile("video")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't find video file data in request", err)
+		return
+	}
+	defer videoData.Close()
+
+	contentType := videoHeader.Header.Get("Content-Type")
+	mediaType, _, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't parse mime type", err)
+		return
+	}
+
+	switch mediaType {
+	case "video/mp4":
+	default:
+		respondWithError(w, http.StatusBadRequest, "Invalid mime type", err)
+		return
+	}
+
+	tempVideoFile, err := os.CreateTemp("", "tubyVideo-")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create temp file", err)
+		return
+	}
+	defer os.Remove(tempVideoFile.Name())
+	defer tempVideoFile.Close()
+
+	_, err = io.Copy(tempVideoFile, videoData)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't write to temp file", err)
+		return
+	}
+	
+	tempVideoFile.Seek(0, io.SeekStart)
+
+	videoAspectRatio, err := getVideoAspectRatio(tempVideoFile.Name())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to get video metadata", err)
+		return
+	}
+
+	videoHexID := make([]byte, 32)
+	rand.Read(videoHexID)
+	title := base64.RawURLEncoding.EncodeToString(videoHexID)
+	extension := strings.TrimPrefix(mediaType, "video/")
+	videoS3Key := fmt.Sprintf("%s/%s.%s", videoAspectRatio, title, extension)
+	
+	params := s3.PutObjectInput {
+		Bucket: &cfg.s3Bucket,
+		Key: &videoS3Key,
+		Body: tempVideoFile,
+		ContentType: &mediaType,
+	}
+	_, err = cfg.s3Client.PutObject(context.Background(), &params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to write to S3", err)
+		return
+	}
+
+	videoAWSURL := fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, videoS3Key)
+	videoMetaData.VideoURL = &videoAWSURL
+	err = cfg.db.UpdateVideo(videoMetaData)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to update database", err)
+		return
+	}
+	respondWithJSON(w, http.StatusNoContent, "")
+}
