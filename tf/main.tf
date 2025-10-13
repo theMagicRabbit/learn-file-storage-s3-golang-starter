9419489f6af5fcc1f683c0fc72acbb9b2074e2ea
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-10-13 18:02:09 -0500

    Use TF to create user policies

--- tf/main.tf
+++ tf/main.tf
@@ -17,6 +17,10 @@ resource "aws_s3_bucket_public_access_block" "allow_all_access" {
   restrict_public_buckets = false
 }
 
+resource "aws_iam_group" "managers_group" {
+  name = "managers"
+}
+
 resource "aws_s3_bucket_policy" "allow_get_access" {
   bucket = aws_s3_bucket.learning_bucket.id
   policy = <<EOP
@@ -33,3 +37,40 @@ resource "aws_s3_bucket_policy" "allow_get_access" {
 }
 EOP
 }
+
+resource "aws_iam_policy" "managers_from_home_policy" {
+  name        = "manager-from-home"
+  path        = "/"
+  description = "Allows manager group access from home IP"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid      = "VisualEditor0"
+        Effect   = "Allow"
+        Action   = "*"
+        Resource = "*"
+        Condition = {
+          IpAddress = {
+            "aws:SourceIp" = "0.0.0.0/32"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_group_policy_attachment" "attach_manager_from_home_to_manager" {
+  group      = aws_iam_group.managers_group.name
+  policy_arn = aws_iam_policy.managers_from_home_policy.arn
+}
+
+resource "aws_iam_group_membership" "managers_members" {
+  name = "tubely-managers-group-memberships"
+
+  users = ["tofu", "brt"]
+
+  group = aws_iam_group.managers_group.name
+}
+
